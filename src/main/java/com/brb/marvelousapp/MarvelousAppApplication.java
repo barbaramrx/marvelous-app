61d4957da8ce55a4fd59b8a683dd5faeb3e444c0
Barbara Marques <59567593+barbaramrx@users.noreply.github.com>
2021-05-21 10:26:40 -0300

    initial commit

---
+++ src/main/java/com/brb/marvelousapp/MarvelousAppApplication.java
@@ -0,0 +1,13 @@
+package com.brb.marvelousapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelousAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelousAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brb/marvelousapp/MarvelousAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brb.marvelousapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelousAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
